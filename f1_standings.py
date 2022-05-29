a046a862e78294f9deed4d079e73547140842fed
ruwinda-nanayakkara <ruwindananayakkara@gmail.com>
2022-05-29 11:40:09 +0530

    Extracted full name in order

--- f1_standings.py
+++ f1_standings.py
@@ -1,6 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
+#Link for f1 officila site 
 source = requests.get("https://www.formula1.com/en/results.html/2022/drivers.html").text
-print(source)
+soup = BeautifulSoup(source,'lxml')
 
+#Extracted data
+first_names = soup.find_all('span', class_ = 'hide-for-tablet')
+last_names = soup.find_all('span',class_ = 'hide-for-mobile')
+#teams = soup.find_all('a',class_ = "grey semi-bold uppercase ArchiveLink")
+
+for i in range(len(first_names)):
+    print("{} {}".format(first_names[i].string,last_names[i].string))
