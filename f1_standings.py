1c210722277a2bc4b700064e48228d80a1c6617c
ruwinda-nanayakkara <ruwindananayakkara@gmail.com>
2022-05-29 12:17:00 +0530

    Error handeling with try except

--- f1_standings.py
+++ f1_standings.py
@@ -6,10 +6,15 @@ source = requests.get("https://www.formula1.com/en/results.html/2022/drivers.htm
 soup = BeautifulSoup(source,'lxml')
 
 #Extracted data
-first_names = soup.find_all('span', class_ = 'hide-for-tablet')
-last_names = soup.find_all('span',class_ = 'hide-for-mobile')
-teams = soup.find_all('a',class_ = "grey semi-bold uppercase ArchiveLink")
-points = soup.find_all('td', class_ = "dark bold")
+try:
+    first_names = soup.find_all('span', class_ = 'hide-for-tablet')
+    last_names = soup.find_all('span',class_ = 'hide-for-mobile')
+    teams = soup.find_all('a',class_ = "grey semi-bold uppercase ArchiveLink")
+    points = soup.find_all('td', class_ = "dark bold")
 
-for i in range(len(first_names)):
-    print("{} {} {} {}".format(first_names[i].string,last_names[i].string,teams[i].string,points[i].string))
+    for i in range(len(first_names)):
+        print("{} {} {} {}".format(first_names[i].string,last_names[i].string,teams[i].string,points[i].string))
+except:
+    print("Error: NOT FOUND")
+
+
